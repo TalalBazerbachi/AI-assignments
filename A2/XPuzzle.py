a15cd9fd3a3b82745dc8cc39f5dc514ecab610e2
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-11-17 18:00:54 -0500

    Add Puzzle class with a constructor and a slideDown method

---
+++ A2/XPuzzle.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class XPuzzle:
+    initialState = np.array([])
+    # GOALSTATE1 = np.array([[1,2,3,4],[5,6,7,0]])
+    # GOALSTATE2 = np.array([[1,3,5,7],[2,4,6,0]])
+    emptyTilePosition = [0,0]
+
+    def __init__(self, initState):
+        self.initialState = initState
+        tempEmptyTilePosition = np.where(initState == 0)
+        self.emptyTilePosition[0] = tempEmptyTilePosition[0][0]
+        self.emptyTilePosition[1] = tempEmptyTilePosition[1][0]
+    
+    ###### Operators ######
+    def slideDown(self):
+        #the empty tile is at the bottom of the matrix
+        if((len(self.initialState)-1) == self.emptyTilePosition[0]):
+            return None
+
+        newState = np.copy(self.initialState)
+        newState[self.emptyTilePosition[0]][self.emptyTilePosition[1]], newState[self.emptyTilePosition[0]+1][self.emptyTilePosition[1]] = newState[self.emptyTilePosition[0]+1][self.emptyTilePosition[1]], newState[self.emptyTilePosition[0]][self.emptyTilePosition[1]]
+
+        return newState
+
+    
+
+
+initStt = np.array([[0,2,1,3],[7,4,6,5]])
+x = XPuzzle(initStt)    
+print(x.slideDown() )
+print(x.initialState)
+# print(x.initialState)
