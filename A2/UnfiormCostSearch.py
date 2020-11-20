88011fca775982555861fc53f70ce9b4253b7c20
Talal Bazerbachi <bazerbachi.talal@gmail.com>
2020-11-19 22:34:47 -0500

    update repository

--- A2/UnfiormCostSearch.py
+++ A2/UnfiormCostSearch.py
@@ -1,147 +1,152 @@
 import time
 import numpy as np
-
-# from output_writer import OutputWriter
 import UCNode
-# from algorithms.helper import Helper
-# from algorithms.helper import get_state_as_string
 import XPuzzle
 
-
+# The class uniform cost is used to solve puzzles using Uniform cost model
 class UniformCost:
-    """
-    This class is responsible for executing the "uniform cost" search algorithm on the provided puzzle instance.
-    """
-
-    def __init__(self, currentPuzzleNum, puzzle, puzzleRows):
+    def __init__(self, currentPuzzleNum, puzzle):
+        # puzzle is the intial puzzle passed
         self.puzzle = puzzle
-        self.timeout = 60
-        # self.helper = Helper(len(puzzle), puzzleRows)
-        self.xpuzzle = XPuzzle.XPuzzle(puzzle)
-        print("am here!!!")
-        # self.output_writer = OutputWriter(currentPuzzleNum, "ucs")
-        self.openList = [UCNode.UCNode(puzzle, None, 0, None)]
+        
+        # the attribute pNumber is used to write the names of the output files
+        self.pNumber = currentPuzzleNum
+
+        # open and closed list are used during findIng the goal puzzle
+        self.openList = [UCNode.UCNode(puzzle, None, 0,0)]
         self.closedList = []
 
-    def solve(self):
+    # The function findGoal is used to find the goal state if it exists
+    def findGoal(self):
         initialTime = time.time()
         currentTime = 0
-        timedout = False
         goalFound = False
         goalPuzzle = None 
-        openListNotEmpty = len(self.openList) > 0
-        modelTimedOut = currentTime > self.timeout
-        
-        while  openListNotEmpty and not modelTimedOut:
-            if currentTime>self.timeout:
-                "model is taking too long to build"
-                break
+
+
+        while  (len(self.openList) > 0) and not (currentTime > 60):
+
             currentPuzzle = self.openList.pop(0)
             self.closedList.append(currentPuzzle)
 
-            if self.xpuzzle.isGOAL1(currentPuzzle.state) or self.xpuzzle.isGOAL2(currentPuzzle): 
+            # first we check if the current state is the goal state
+            if XPuzzle.isGOAL1(np.array(currentPuzzle.state)) or XPuzzle.isGOAL2(np.array(currentPuzzle)): 
                 goalFound = True
-                goalPuzzle = currentPuzzle
+                self.goalPuzzle = currentPuzzle
                 break
             
-            succesors = self.getSucessors(self.xpuzzle, currentPuzzle.state, currentPuzzle)
-            self.pushPossibleSucessors(currentPuzzle, succesors)
+            # if it's not the goal state we generate it's sucessors and sort them based on the cost
+            succesors = self.getSucessors(currentPuzzle)
             self.openList.sort(key=lambda node: node.cost)
+
             currentTime = time.time() - initialTime
 
+        # At this point the function either found the goal or it didn't 
+        # (either because the goal is unreachable or it took more than 60 seconds)
+        f = open("50 puzzles output files/"+str(self.pNumber)+" ucs solution.txt", "w")
+        searchWriter = open("50 puzzles output files/"+str(self.pNumber)+" ucs search.txt", "w")        
+        #to write the solution first I need to generate it by adding all the accepted state to a list 
+        # every time I reach a new state I change the 'currentState attribute to be the parent of the previuos current state
+        # until we reach teh initial state were the parent is None
+        solution=[]
         if goalFound:
-            print("goal is found! \n")
+            i=0
+            currentPuzzle=self.goalPuzzle
+            while True:
+                if currentPuzzle.parent is not None:
+                    solution.append(currentPuzzle)
+                    currentPuzzle=currentPuzzle.parent
+                else :
+                    solution.append(currentPuzzle)
+                    break
+                i=i+1
+            
+            # In this loop I am adding the solution path to it's uotput file
+            count = len(solution)-1
+            while count>=0:
+                toWrite=str(solution[count].tileMoved)+" "+ str(solution[count].cost)+" "+str(solution[count].state)+"\n"
+                toWrite = toWrite.replace("[", "")
+                toWrite = toWrite.replace("]", "")
+                toWrite = toWrite.replace("\n", "")
+                f.write(toWrite)
+                f.write("\n")
+                count-=1
+            
+            # In this section I am adding the searched state (states in the closed list)
+            count =0
+            while count <= len(self.closedList)-1:
+                toWrite=str(self.closedList[count].cost)+" "+ str(self.closedList[count].cost)+" "+str(0)+" "+str(self.closedList[count].state)+"\n"
+                toWrite = toWrite.replace("[", "")
+                toWrite = toWrite.replace("]", "")
+                toWrite = toWrite.replace("\n", "")                
+                searchWriter.write(toWrite)
+                searchWriter.write("\n")
+                count+=1
 
         else:
-            if currentTime >= self.timeout:
+            f.write("no solution\n")
+            searchWriter.write("no solution\n")
 
-                print("model timed out, goal state was not found")
-            else:
-                print("goal state was not found")
+        b =currentTime>60
+        return len(solution), len(self.closedList), currentTime, b
 
-  
-        print("Uniform Cost Search is done, it took"+currentTime)
+    # AddSuccessor is used to add a new node to the open list if it's not their and it's not in the closed list 
+    # if the current state's cost is less than the same state in the open list we swap them
+    def AddSuccessor(self,sNode):
+        newNode = UCNode.UCNode(sNode[0], sNode[1], sNode[2], sNode[3])
+        inClosedList = self.checkInClosedList(newNode)
+        inOpenList = self.checkInOpenList(newNode)
+        if not inClosedList and not inOpenList:
+            self.openList.append(newNode)  
 
+    # This functio is used to generate possible successors based on the functions of XPuzzle
+    def getSucessors(self, currentPuzzle):
+        puzzleO=XPuzzle.XPuzzle(currentPuzzle.state)
 
-    def getSucessors(self, xpuzzle, currentPuzzleState, currentPuzzle):
-        movesList = []
+        if puzzleO.slideDown()[0]is not None:
+            sNode=puzzleO.slideDown()[0],currentPuzzle, currentPuzzle.cost+1,puzzleO.slideDown()[1]
+            self.AddSuccessor(sNode)
 
-        # Generate Possible sucessors of the current state based on possible moves allowed
-        print("parent node of the new sucessors: ")
-        ts = currentPuzzleState.tostring()
-        print(np.fromstring(ts, dtype=int))
-        moveUpCopy = currentPuzzleState.copy()
-        upPuzzle=XPuzzle.XPuzzle(moveUpCopy)
-        if upPuzzle.slideUp()is not None:
-            movesList.append((upPuzzle.slideUp(),currentPuzzleState, currentPuzzle.cost+1, 'move_up'))
-            ts = upPuzzle.slideUp().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
+        if puzzleO.slideUp()[0]is not None:
+            sNode = puzzleO.slideUp()[0],currentPuzzle, currentPuzzle.cost+1,puzzleO.slideUp()[1]
+            self.AddSuccessor(sNode)
 
-        moveDownCopy = currentPuzzleState.copy()
-        downPuzzle=XPuzzle.XPuzzle(moveDownCopy)
-        if downPuzzle.slideDown()is not None:
-            movesList.append((downPuzzle.slideDown(),currentPuzzleState, currentPuzzle.cost+1, 'moveDown'))
-            ts = downPuzzle.slideDown().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
+        if puzzleO.slideRight()[0]is not None:
+            sNode= puzzleO.slideRight()[0],currentPuzzle, currentPuzzle.cost+1,puzzleO.slideRight()[1]
+            self.AddSuccessor(sNode)
 
-        moveLeftCopy = currentPuzzleState.copy()
-        leftPuzzle=XPuzzle.XPuzzle(moveLeftCopy)
-        if leftPuzzle.slideLeft()is not None:
-            movesList.append((leftPuzzle.slideLeft(),currentPuzzleState, currentPuzzle.cost+1, 'moveLeft'))
-            ts = leftPuzzle.slideLeft().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
+        if puzzleO.slideLeft()[0]is not None:
+            sNode = puzzleO.slideLeft()[0],currentPuzzle, currentPuzzle.cost+1,puzzleO.slideLeft()[1]
+            self.AddSuccessor(sNode)
 
-        moveRightCopy = currentPuzzleState.copy()
-        rightPuzzle=XPuzzle.XPuzzle(moveRightCopy)
-        if rightPuzzle.slideRight()is not None:
-            movesList.append((rightPuzzle.slideRight(),currentPuzzleState, currentPuzzle.cost+1, 'move_Rght'))
-            ts = rightPuzzle.slideRight().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
-
-        wrappingSlideCopyCopy = currentPuzzleState.copy()
-        wrapPuzzle=XPuzzle.XPuzzle(wrappingSlideCopyCopy)
-        if wrapPuzzle.wrappingSlide()is not None:
-            movesList.append((xpuzzle.wrappingSlide(),currentPuzzleState, currentPuzzle.cost+1, 'wrappingSlide'))
-            ts = wrapPuzzle.wrappingSlide().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
-            
-        
-        diagonalSlideCopy = currentPuzzleState.copy()
-        diagonalPuzzle=XPuzzle.XPuzzle(diagonalSlideCopy)
-        if diagonalPuzzle.diagonalSlideInside()is not None:
-            movesList.append((diagonalPuzzle.diagonalSlideInside(),currentPuzzleState, currentPuzzle.cost+1, 'diagonalSlideInside'))
-            ts = diagonalPuzzle.diagonalSlideInside().tostring()
-            print(np.fromstring(ts, dtype=int))
-            print("\n")
-        return movesList
-
-
-    def pushPossibleSucessors(self, node, sucessors):
-
-        for s in sucessors:
-            sNode = UCNode.UCNode(s[0], s[1], s[2],(s[0] - s[1]))
-
-            inClosedList = False
-            for closedNosed in self.closedList:
-                if (np.array(sNode.state) == np.array(closedNosed.state)).all():
-                    inClosedList = True
-                    break
+        if puzzleO.wrappingSlide()[0]is not None:
+            sNode = puzzleO.wrappingSlide()[0],currentPuzzle, currentPuzzle.cost+2,puzzleO.wrappingSlide()[1]
+            self.AddSuccessor(sNode)
  
-            inOpenList = False
-            if not inClosedList:
-                for i in range(len(self.openList)):
-                    closedNosed = self.openList[i]
-                    if (np.array(sNode.state) == np.array(closedNosed.state)).all():
-                        if closedNosed.cost < sNode.cost:
-                            inOpenList = True
-                        else:
-                            self.openList[i] = sNode
-                        break
+        if puzzleO.diagonalSlideInside()[0]is not None:
+            sNode = puzzleO.diagonalSlideInside()[0],currentPuzzle, currentPuzzle.cost+3,puzzleO.diagonalSlideInside()[1]
+            self.AddSuccessor(sNode)
 
-            if not inClosedList and not inOpenList:
-                self.openList.append(sNode)
+        if puzzleO.diagonalSlideOpposed()[0]is not None:
+            sNode = puzzleO.diagonalSlideOpposed()[0],currentPuzzle, currentPuzzle.cost+3,puzzleO.diagonalSlideOpposed()[1]
+            self.AddSuccessor(sNode)
+
+    # This function is used to check if a state is in the closed list
+    def checkInClosedList(self,toCompare):
+        for i in range(len(self.closedList)):
+            if np.array_equal(self.closedList[i].state,toCompare.state):
+                return True
+        return False
+
+    # This function is used to check if a state is in the open list
+    # If a state is in the open list and the incoming state has a lower cost the old version is romved
+    # and the incoming state is added to the open list
+    def checkInOpenList(self,toCompare):
+        for i in range(len(self.openList)):
+            if np.array_equal(self.openList[i].state,toCompare.state):
+                if (toCompare.cost < self.openList[i].cost):
+                    openList[i]=toCompare
+                return True
+            else:
+                return False
+        
--- A2/main.py
+++ A2/main.py
@@ -3,39 +3,62 @@ import numpy as np
 import glob
 import time
 import UnfiormCostSearch
-def read_samples(input_filename):
-    """
-    Read in the sample initial states.
-    :param input_filename: The filename to use for reading-in the data.
-    :return: The array of samples.
-    """
-    # Define an array to store the initial puzzle states
-    initial_states = []
-
-    # Read the sample initial-states from the input file line-by-line and add
-    # each one into our array
-    file = open(input_filename, "r")
-
-    while True:
-        # Get the next line from the file
-        line = file.readline()
-
-        # If the line is empty, we are done reading
-        if not line:
-            break
-
-        # Write our sample initial-state to our array (strip out the newline character)
-        initial_states.append(line.strip())
-    # end: while
-
-    # Don't forget to close our file connection
-    file.close()
-
-    return initial_states
+import readInputFile
 
 
-print("am here")
-input_file = "inputs/sample_inputs.txt"
-sample_puzzles = read_samples(input_file)
-uniformcost = UnfiormCostSearch.UniformCost(0, np.array([[3, 0, 1, 4], [2, 6, 5, 7]]) , 2)
-uniformcost.solve()
+puzzles=readInputFile.getPuzzles("radomInput.txt",(2,4))
+
+i=0
+# Analysis array is used  to store information used for the model analysis
+analysis=[]
+
+for puzzle in puzzles:
+    print("trying to solve puzzle "+ str(i)+"...")
+    puzzle=np.array(puzzle)
+    uniformcost = UnfiormCostSearch.UniformCost(i, puzzle)
+    analysis.append(uniformcost.findGoal())
+    i+=1
+
+# the arrays and attributes are used to caclulate the average and total values of anaylsis requirements
+solutionPath=[]
+solPath=0
+
+searchPath=[]
+sPath=0
+
+execution=[]
+totalExecution=0
+
+noSol=[]
+totalNoSol=0
+for a in analysis:
+    solutionPath.append(a[0])
+    solPath+=a[0]
+
+    searchPath.append(a[1])
+    sPath+=a[1]
+
+    execution.append(a[2])
+    totalExecution+=a[2]
+
+    noSol.append(a[3])
+
+
+analysisWRiter = open("ModelAnalysis/ ucs analysis.txt", "w")
+analysisWRiter.write("Data For Solution Path:\n\n")
+analysisWRiter.write("total solution path:\n")
+analysisWRiter.write(str(solPath))
+analysisWRiter.write("\n\navrage solution path:\n")
+analysisWRiter.write(str(solPath/len(solutionPath)))
+analysisWRiter.write("total search path:\n")
+analysisWRiter.write(str(sPath))
+analysisWRiter.write("\n\navrage search path:\n")
+analysisWRiter.write(str(sPath/len(searchPath)))
+analysisWRiter.write("\ntotal no solution :\n")
+analysisWRiter.write(str(noSol.count(True)))
+analysisWRiter.write("\ntotal no solution :\n")
+analysisWRiter.write(str(noSol.count(True)/len(noSol)))
+analysisWRiter.write("\n\ntotal execution time:\n")
+analysisWRiter.write(str(totalExecution))
+analysisWRiter.write("\n\naverage execution path:\n")
+analysisWRiter.write(str(sPath/len(execution)))
